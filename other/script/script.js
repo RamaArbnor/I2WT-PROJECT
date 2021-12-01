34edc2067b721a1100088f1e5b9284a04c48849b
GalicaYlber <ygalica@york.citycollege.eu>
2021-12-01 18:23:06 +0200

    Montage Theme

---
+++ other/script/script.js
@@ -0,0 +1,17 @@
+function showNav(){
+	var nav = document.getElementById('nav_bar')
+	var montage = document.getElementById('bg_img')
+	// console.log(montage.style.opacity)
+
+	montage.style.opacity = 1 + window.scrollY / -650
+
+	// console.log(montage.style.opacity)
+	// console.log(window.scrollY)
+
+	nav.classList.add('shown')
+
+	if (window.scrollY == 0) {
+		nav.classList.remove('shown')
+	}
+}
+
