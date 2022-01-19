0ec3179739cdcf823a4db30b3134678489454955
GalicaYlber <ygalica@york.citycollege.eu>
2022-01-19 12:43:06 +0200

    Cartoons Final Version

--- other/script/principles.js
+++ other/script/principles.js
@@ -1,10 +1,5 @@
 var to_top = document.getElementById('to_top')
 
-// Hover Card
-function hoverCard() {
-
-}
-
 // Show Back to Top arrow on scroll, hide when on top
 function showArrow() {
 	to_top.style.top = '85vh'
